62eeca7c951ce7b6be6e75ab1b27cf3e679cc000
francescoaristei <francesco.aristei@outlook.it>
2024-06-12 16:00:56 +0200

    implement rio_writen to write response to conn_fd

--- app/server.c
+++ app/server.c
@@ -7,17 +7,34 @@
 #include <errno.h>
 #include <unistd.h>
 
-int main() {
+
+ssize_t rio_writen (int fd, void *buf, size_t n) {
+	size_t nleft = n;
+	ssize_t nwritten;
+
+
+	while (nleft > 0) {
+		if ((nwritten = write(fd, buf, nleft)) <= 0) {
+			if (errno == EINTR)
+				nwritten = 0;
+			else
+				return - 1;
+		}
+		nleft -= nwritten;
+		buf += nwritten;
+	}
+
+	return n;
+}
+
+int main () {
 	// Disable output buffering
 	setbuf(stdout, NULL);
  	setbuf(stderr, NULL);
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
+	char *bufResponse = "HTTP/1.1 200 OK\r\n\r\n";
 
-	// Uncomment this block to pass the first stage
-	//
-	int server_fd, client_addr_len;
+	int server_fd, client_addr_len, conn_fd;
 	struct sockaddr_in client_addr;
 	
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -26,8 +43,7 @@ int main() {
 	 	return 1;
 	}
 	
-	// Since the tester restarts your program quite often, setting SO_REUSEADDR
-	// ensures that we don't run into 'Address already in use' errors
+	// SO_REUSEADDR ensures that we don't run into 'Address already in use' errors
 	int reuse = 1;
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
 		printf("SO_REUSEADDR failed: %s \n", strerror(errno));
@@ -53,8 +69,12 @@ int main() {
 	printf("Waiting for a client to connect...\n");
 	client_addr_len = sizeof(client_addr);
 	
-	accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	conn_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
+
+	ssize_t nres = rio_writen(conn_fd, bufResponse, strlen(bufResponse));
+	
+	close(conn_fd);
 	close(server_fd);
 
 	return 0;
