2a5fa423088d1e55495b04526356d96e27efc802
francescoaristei <francesco.aristei@outlook.it>
2024-06-13 11:22:55 +0200

    parsing client request

--- app/server.c
+++ app/server.c
@@ -7,7 +7,78 @@
 #include <errno.h>
 #include <unistd.h>
 
+# define BUF_SIZE 8192
+# define MAX_LINE 8092
 
+/* struct that defines an internal buffer where to read/write avoiding frequent traps to OS */
+typedef struct {
+	int rio_fd;
+	int rio_cnt;
+	char buf[BUF_SIZE];
+	char *rio_bufptr;
+} rio_t;
+
+
+/* initialize rio_t internal buffer */
+void rio_init (rio_t *riot, int fd) {
+	riot->rio_fd = fd;
+	riot->rio_cnt = 0;
+	riot->rio_bufptr = riot->buf;
+}
+
+/* buffered version of read(), reads from the internal buffer rio_t -> buf until is not completely consumed */
+ssize_t rio_read (rio_t *riot, char *usrbuf, size_t n) {
+	int cnt;
+	while (riot->rio_cnt <= 0) {
+		riot->rio_cnt = read(riot->rio_fd, riot->buf, sizeof(riot->buf));
+		if (riot->rio_cnt < 0) {
+			if (errno != EINTR) { // sighandler
+				return -1;
+			} 
+		} else if (riot->rio_cnt == 0) { // EOF
+			return 0;
+		} else {
+			// reset buffer
+			riot->rio_bufptr = riot->buf;
+		}
+	}
+
+	cnt = n;
+	if (riot->rio_cnt < n) {
+		cnt = riot->rio_cnt;
+	}
+	memcpy(usrbuf, riot->rio_bufptr, cnt);
+	riot->rio_bufptr += cnt;
+	riot->rio_cnt -= cnt;
+	return cnt;
+}
+
+/* improved version of read, avoids short count, reads until requested bytes are read (n) or EOF */
+size_t rio_readnb (rio_t  *riot, void *usrbuf, size_t n) {
+	size_t nleft = n;
+	ssize_t nread;
+	char *buf = usrbuf;
+
+	while (nleft > 0) {
+		nread = rio_read(riot, buf, nleft);
+		if (nread < 0) {
+			if (errno == EINTR) { // interrupted by sighandler, call read() again
+				nread = 0;
+			} else {
+				return -1; // error
+			}
+		} else if (nread == 0)  { // EOF
+			break;
+		}
+		nleft -= nread;
+		buf += nread;
+	}
+
+	return (n - nleft);
+}
+
+
+/* buffered write, used to avoid dealing with short counts encountered in network applications due to network delay etc. */
 ssize_t rio_writen (int fd, void *buf, size_t n) {
 	size_t nleft = n;
 	ssize_t nwritten;
@@ -27,12 +98,25 @@ ssize_t rio_writen (int fd, void *buf, size_t n) {
 	return n;
 }
 
+
+void parseRequest (char *requestBuf, char *responseBuf) {
+	printf("%s\n", requestBuf);
+}
+
+
 int main () {
 	// Disable output buffering
 	setbuf(stdout, NULL);
  	setbuf(stderr, NULL);
 
-	char *bufResponse = "HTTP/1.1 200 OK\r\n\r\n";
+	// internal buffer to read from client
+	rio_t riot;
+	// number of bytes read
+	size_t n;
+
+	// buffer to read request
+	char bufRequest[MAX_LINE];
+	char *bufResponse[MAX_LINE];
 
 	int server_fd, client_addr_len, conn_fd;
 	struct sockaddr_in client_addr;
@@ -71,6 +155,16 @@ int main () {
 	
 	conn_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
 	printf("Client connected\n");
+
+	// initialize internal buffer to read from conn_fd
+	rio_init(&riot, conn_fd);
+
+	// read request into bufRequest
+	while ((n = rio_readnb(conn_fd, bufRequest, MAX_LINE) != 0)) {
+		printf("%d bytes read by the server\n", n);
+	}
+
+	parseRequest(bufRequest, bufResponse);
 
 	ssize_t nres = rio_writen(conn_fd, bufResponse, strlen(bufResponse));
 	
