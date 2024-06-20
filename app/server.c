b3b698f92ef8f2535be4b31a629dec4be7d57733
francescoaristei <francesco.aristei@outlook.it>
2024-06-20 16:34:51 +0200

    gzip compression algorithm implementation via zlib

--- app/server.c
+++ app/server.c
@@ -264,7 +264,7 @@ void echo_endpoint (char *bufResponse, char *ptr, char *response, char *encoding
             char compressed[MAX_LINE];
             size_t compressed_length = sizeof(compressed);
             if (gzip(response, sizeof(response), compressed, &compressed_length) == Z_OK) {
-                sprintf(bufResponse, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: %zu\r\n\r\n", i);
+                sprintf(bufResponse, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: %zu\r\n\r\n", compressed_length);
                 memcpy(bufResponse + strlen(bufResponse), compressed, compressed_length);
                 *resp_len = strlen(bufResponse) + compressed_length;
             }
